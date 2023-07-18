c61768688c3657bc4c99c175b69194fe4689393f
renan <renancarvalhodesouza@gmail.com>
2023-07-17 21:55:33 -0300

    chor: change code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,7 +21,7 @@ describe('AppComponent', () => {
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
+    const compiled: HTMLElement = fixture.nativeElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('dashboard app is running!');
   });
 });
